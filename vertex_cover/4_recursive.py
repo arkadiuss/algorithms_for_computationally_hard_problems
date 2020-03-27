38c69428c9d510c10f5112c184e3db9e8dedcb92
arkadius <arkadius.kraus@gmail.com>
2020-03-27 20:16:55 +0100

    fixes, tweaking performance

--- vertex_cover/4_recursive.py
+++ vertex_cover/4_recursive.py
@@ -3,19 +3,22 @@ from dimacs import *
 from itertools import combinations
 
 def all_in(G, S):
+    v = None
     for u in range(1,len(G)):
         Su = G[u]
-        if len(Su) > 2 and len(Su & S) == 0:
-            return u 
-    return None    
+        if len(Su) > 2:
+            if v == None or len(Su) > len(G[v]):
+                v=u 
+    return v   
 
 def neigh(G, u):
     return G[u]
 
 def filtered(G, S):
-    Gc = G.copy()
+    Gc = [set()]*len(G)
     for u in range(1, len(G)):
-        Gc[u] = G[u].copy() - S
+        if u not in S:
+            Gc[u] = G[u].copy() - S
     for s in S:
         Gc[s]=set()
     return Gc
@@ -32,7 +35,10 @@ def find_two(G):
             return i
     return None
 
-def poly(G):
+def poly(G, k):
+    c = len([1 for i in G if len(i)>1])
+    if c/2 >= k:
+        return None
     R = set()
     Gc = G.copy()
     while True:
@@ -40,24 +46,38 @@ def poly(G):
         if o == None:
             o = find_two(Gc)
         if o == None:
-            break;
+            break
+        if k <= 0:
+            return None
         Gc = filtered(Gc, {o})
         R.add(o)
+        k = k -1
     return R
 
 def vertex_cover(G, k, S):
+    #R = kernelize(G,k)
+    #if R == None:
+    #    return None
+    #G, RS = R
+    #k -= len(RS) 
+    if k<0:
+        return None
     u = all_in(G, S)
     if u == None:
-        return S|poly(G)
+        P = poly(G,k)
+        if P == None:
+            return None
+        return S|P
     if k < 1:
         return None
     Nu = neigh(G, u)
+    S2 = vertex_cover(filtered(G, Nu), k-len(Nu), S|Nu)
+    if S2 != None:
+        return S2
     S1 = vertex_cover(filtered(G, {u}), k-1, S|{u})
     if S1 != None:
         return S1
-    S2 = vertex_cover(filtered(G, Nu), k-1, S|Nu)
-    if S2 != None:
-        return S2
+
 
 def kernelize(G,k):
     Gc = G.copy()
@@ -85,12 +105,12 @@ def kernelize(G,k):
     return Gc, R
 
 def solve_vertex_cover(G):
-    for k in range(1,len(G)):
-        print(k)
+    for k in range(60,len(G)):
         GcR = kernelize(G,k)
         if GcR == None:
             continue
-        S = vertex_cover(GcR[0], k, set())
+        print(k, len(GcR[1]))
+        S = vertex_cover(GcR[0], k-len(GcR[1]), set())
         if S != None:
             return S|GcR[1]
     return []
